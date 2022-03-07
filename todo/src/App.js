f95e46d2e4580eda79b6f9c2dc1966c72b1cbce3
João Moura <64660866+jmoura11@users.noreply.github.com>
2022-03-07 18:54:05 +0000

    added a new state and filtered todos as completed or not
    created a new state to store the todos that are uncompleted and completed

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //Importing Component
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  //state
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //useEffect
+  useEffect(() => {
+    filterHandler();
+  }, [todos,status]);
+  //function
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
@@ -17,8 +38,9 @@ function App() {
         todos={todos}
         setTodos={setTodos}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
     </div>
   );
 }
